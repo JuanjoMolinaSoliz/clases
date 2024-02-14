10ff75bdc5122ff173495cb6016bc7f7a394e120
JuanjoMolinaSoliz <juanjose.m.s@hotmail.com>
2024-02-14 17:01:06 -0400

    proyect start

---
+++ clase1.py
@@ -0,0 +1,11 @@
+print('hola mundo')
+
+nombre = "juan jose"
+apellido = "molina soliz"
+
+print(nombre, apellido)
+
+print (nombre.capitalize(), apellido.title())
+
+print (f"nombre:{nombre} , apellido:{apellido}")
+print (f"Nombre:{nombre}\n  Apellido:{apellido}")
