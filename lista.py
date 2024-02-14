964cfac6f813694350b8383ab1ca16e66525437b
JuanjoMolinaSoliz <juanjose.m.s@hotmail.com>
2024-02-14 18:22:51 -0400

    :wq

---
+++ lista.py
@@ -0,0 +1,11 @@
+#Crear una lista
+paises = ["Bolivia", "Peru", "Chile", "Ecuador", "argentina", "brazil"]
+print(paises)
+print(paises.sort(reverse=True))
+print(paises)
+print(paises.sort(reverse=False))
+#Modificar una lista
+
+#Mostrar los valores de una lista
+
+#Elimivar valores de una lista
