75bca82a82c2927db0155bcca13181d207457cc5
JuanjoMolinaSoliz <juanjose.m.s@hotmail.com>
2024-02-15 17:48:04 -0400

    nueva clase de lastas

---
+++ trabajando_listas.py
@@ -0,0 +1,20 @@
+#lista de equipos de futbol
+
+equipos = ['Aurora', 'Bolivar', 'U de vinto', 'Wilsterman', 'Oriente Petrolero', 'The Strongers', 'Blooming']
+print(equipos)
+
+for equipo in equipos:
+    print(f"Nombre de equipo: {equipo}")
+
+for valor in range(10):
+    print(valor)
+
+for i in range(1,20):
+    print(i)
+
+for i in range(1,100,3):
+    print(i)
+    
+# slicing
+
+print(equipos[1:4])
