3d111b0d27f203fc02cbe03563d638f910309f2e
JuanjoMolinaSoliz <juanjose.m.s@hotmail.com>
2024-02-15 16:00:26 -0400

    nueva clase de lastas

--- lista.py
+++ lista.py
@@ -1,13 +1,52 @@
-#Crear una lista
-paises = ["Bolivia", "Peru", "Chile", "Ecuador", "argentina", "brazil"]
-print(paises)
-paises.sort(reverse=True)
-print(paises)
-paises.sort(reverse=False)
-print(paises)
-#esto es una prueba
+#Crear una lista vacia
+
+lista_vacia = []
+print(len(lista_vacia))
+
+#crear una lista como array
+
+motocicletas = ['Zeus','Pegasus','kyngo','Susuki','BMW', 'Honda']
+
+#crear una lista usando list
+
+paises = list(('España','Portugal','Italia','Francia','Suiza','Alemania'))
+
+#crear lista con rage
+
+numeros = list(range(1,100))
+
 #Modificar una lista
 
-#Mostrar los valores de una lista
+print(paises)
+print(paises[2])
 
-#Elimivar valores de una lista
+paises[2] = 'Italy'
+print(paises)
+print(paises[2])
+
+#Agregar valores append --> agrega items al final de la lista
+
+paises.append('Ucrania')
+paises.append('Rusia')
+
+print(paises)
+
+#agregar valores N posicion
+
+print(motocicletas)
+motocicletas.insert(1,'Kawasaki')
+print(motocicletas)
+
+#eliminar ultimos valores
+
+print(len(paises))
+ultimo = paises.pop()
+print(f"Elemento eliminado:{ultimo}")
+print(len(paises))
+
+#eliminar un valor de la posicion n de una lista
+
+print(paises)
+eliminado = paises.pop(2)
+print(f"eliminado:{eliminado}")
+print(paises)
